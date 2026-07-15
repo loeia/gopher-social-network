5a333bec4d39fcaf25bdfce9c1dd43e88be506cf
loeia <aceoheng@gmail.com>
2026-07-15 14:58:33 +0800

    feat: add UserStore and CommentStore

---
+++ internal/store/user.go
@@ -0,0 +1,70 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"strings"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64    `json:"id"`
+	Username  string   `json:"username"`
+	Email     string   `json:"email"`
+	Password  password `json:"-"`
+	CreatedAt string   `json:"created_at"`
+}
+
+type password struct {
+	Text *string
+	Hash []byte
+}
+
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.Text = &text
+	p.Hash = hash
+
+	return nil
+}
+
+type UserStore struct {
+	db *sql.DB
+}
+
+func NewUserStore(db *sql.DB) *UserStore {
+	return &UserStore{
+		db: db,
+	}
+}
+
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+	query := `insert into users (username,password,email) values ($1,$2,$3) returning id,created_at`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	row := s.db.QueryRowContext(ctx, query, user.Username, user.Password.Hash, user.Email)
+	err := row.Scan(&user.ID, &user.CreatedAt)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			switch {
+			case strings.Contains(pgErr.Message, "users_email_key"):
+				return ErrDuplicateEmail
+			case strings.Contains(pgErr.Message, "users_username_key"):
+				return ErrDuplicateUsername
+			}
+		}
+		return err
+	}
+
+	return nil
+}
